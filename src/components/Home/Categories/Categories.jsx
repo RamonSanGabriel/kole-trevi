ec3cbce957ccab874d210e9e2b39563afc38c9b1
RamonSanGabriel <jeshero11@gmail.com>
2025-01-21 02:17:09 +0800

    category buttons

--- src/components/Home/Categories/Categories.jsx
+++ src/components/Home/Categories/Categories.jsx
@@ -16,38 +16,48 @@ const Categories = () => {
       <div className="categoryContainer">
         <hr />
         <ul>
-          <li>
+          <li className="categoryListItem">
             <a href="">
-              <FcAutomotive className="categoryIcons" />
-              {/* <MdToys /> */}
-              <h3>Toys</h3>
+              <button className="categoryBtn">
+                <FcAutomotive className="categoryIcons" />
+                <h3>Toys</h3>
+              </button>
             </a>
           </li>
-          <li>
+
+          <li className="categoryListItem">
             <a href="">
-              {/* <MdOutlineToys /> */}
-              <FcCompactCamera className="categoryIcons" />
-              <h3>Photos</h3>
+              <button className="categoryBtn">
+                <FcCompactCamera className="categoryIcons" />
+                <h3>Photos</h3>
+              </button>
             </a>
           </li>
-          <li>
+
+          <li className="categoryListItem">
             <a href="">
-              {/* <GiClothes /> */}
-              <FcSportsMode className="categoryIcons" />
-              <h3>Events</h3>
+              <button className="categoryBtn">
+                <FcSportsMode className="categoryIcons" />
+                <h3>Events</h3>
+              </button>
             </a>
           </li>
-          <li>
+
+          <li className="categoryListItem">
             <a href="">
-              {/* <MdOutlineFoodBank /> */}
-              <FcReading className="categoryIcons" />
-              <h3>Books</h3>
+              <button className="categoryBtn">
+                <FcReading className="categoryIcons" />
+                <h3>Books</h3>
+              </button>
             </a>
           </li>
-          <li>
+
+          <li className="categoryListItem">
             <a href="">
-              <FcFilmReel className="categoryIcons" />
-              <h3>Movies</h3>
+              <button className="categoryBtn">
+                <FcFilmReel className="categoryIcons" />
+                <h3>Movies</h3>
+              </button>
             </a>
           </li>
         </ul>
