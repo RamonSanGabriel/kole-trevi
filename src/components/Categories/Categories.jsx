7dd347340068d24686bbb288c717e11eb537c634
RamonSanGabriel <jeshero11@gmail.com>
2025-01-22 15:20:51 +0800

    category content

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -8,7 +8,20 @@ const Categories = () => {
   return (
     <div className="categoryWrapper">
       <div className="categoryContainer">
+        {/* <div className="categoryDetails"> */}
         <h1>Category Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          nostrum saepe minus porro vitae voluptatibus nisi facilis in culpa
+          quisquam? Atque consequuntur quasi pariatur vero temporibus.
+          Asperiores eveniet dicta quasi. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Tenetur deserunt veritatis commodi
+          recusandae quasi pariatur omnis totam optio cumque facere. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Fuga commodi
+          reprehenderit, qui tempora ex nesciunt blanditiis maxime a laborum
+          rem.
+        </p>
+        {/* </div> */}
         <hr />
         <form action="">
           <ul>
