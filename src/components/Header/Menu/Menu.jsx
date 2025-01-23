b677cfa563178720e2b960afa0019558481802fc
RamonSanGabriel <jeshero11@gmail.com>
2025-01-24 01:04:42 +0800

    hamburger menu

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -16,49 +16,11 @@ const Menu = () => {
 
   return (
     <>
-      <nav className="navWrapper">
-        {/* <TfiMenu className="menuIcon" /> */}
-        {/*   <button className="menuBtn">
-        <Hamburger className="menuIcon" isOpen={isOpen} setOpen={setIsOpen} />
-        <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
-            className={css.menu}
-          >
-            <ul>
-              {navLinks.map(({ id, path, icon, link }) => {
-                console.log(navLinks);
-                <motion.li
-                  key={id}
-                  initial={{ scale: 0, opacity: 0 }}
-                  animate={{ scale: 1, opacity: 1 }}
-                  transition={{
-                    type: 'spring',
-                    stiffness: 260,
-                    damping: 20,
-                    delay: 0.1 + idx / 10,
-                  }}
-                >
-                  <NavLink to={path} onClick={() => setIsOpen((prev) => !prev)}>
-                    {icon}
-                    {title}
-                    {link}
-                  </NavLink>
-                </motion.li>;
-              })}
-            </ul>
-          </motion.div>
-        )}
-        </AnimatePresence>
-      </button> */}
+      <nav className="menuWrapper">
         <button className="menuBtn" onClick={() => setIsOpen((prev) => !prev)}>
           <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
         </button>
-        <div className="navListContainer">
+        <div className="menuListContainer">
           <AnimatePresence>
             {isOpen && (
               <motion.div
@@ -69,7 +31,7 @@ const Menu = () => {
                 className="menu"
               >
                 {/* <button className="menuBtn"> */}
-                <ul className="navList">
+                <ul className="menuList">
                   {menuRoutes.map(({ idx, title, path, icon }) => {
                     return (
                       <motion.li
@@ -82,7 +44,7 @@ const Menu = () => {
                           delay: 0.1 + idx / 10,
                         }}
                         key={title}
-                        className="navListItem"
+                        className="menuListItem"
                       >
                         <NavLink to={path}>
                           <span>
