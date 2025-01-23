42a91a390b3561b92d77ae1b26a644b607105a11
RamonSanGabriel <jeshero11@gmail.com>
2025-01-23 15:01:27 +0800

    hamburger icon display

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 import { Squash as Hamburger } from 'hamburger-react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useClickAway } from 'react-use';
-import { menuRoutes } from '../../../data/menu-routes';
+// import { menuRoutes } from '../../../data/menu-routes';
 import { useRef, useState } from 'react';
 
 const NavBar = () => {
@@ -16,54 +16,59 @@ const NavBar = () => {
   return (
     <nav className="navWrapper">
       {/* <TfiMenu className="menuIcon" /> */}
-      <button className="menuBtn">
+      {/*   <button className="menuBtn">
         <Hamburger className="menuIcon" isOpen={isOpen} setOpen={setIsOpen} />
         <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2 }}
-              className={css.menu}
-            >
-              <ul>
-                {menuRoutes.map(({ title, path, icon, link }) => (
-                  <motion.li
-                    key={title}
-                    initial={{ scale: 0, opacity: 0 }}
-                    animate={{ scale: 1, opacity: 1 }}
-                    transition={{
-                      type: 'spring',
-                      stiffness: 260,
-                      damping: 20,
-                      delay: 0.1 + idx / 10,
-                    }}
-                  >
-                    <NavLink
-                      to={path}
-                      onClick={() => setIsOpen((prev) => !prev)}
-                    >
-                      {/* {icon} */}
-                      {/* {title} */}
-                      {link}
-                    </NavLink>
-                  </motion.li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className={css.menu}
+          >
+            <ul>
+              {navLinks.map(({ id, path, icon, link }) => {
+                console.log(navLinks);
+                <motion.li
+                  key={id}
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: 'spring',
+                    stiffness: 260,
+                    damping: 20,
+                    delay: 0.1 + idx / 10,
+                  }}
+                >
+                  <NavLink to={path} onClick={() => setIsOpen((prev) => !prev)}>
+                    {icon}
+                    {title}
+                    {link}
+                  </NavLink>
+                </motion.li>;
+              })}
+            </ul>
+          </motion.div>
+        )}
         </AnimatePresence>
+      </button> */}
+      <button className="menuBtn">
+        <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
       </button>
-      {/*      <div className="navListContainer">
+      <div className="navListContainer">
+        {/* <button className="menuBtn"> */}
         <ul className="navList">
           {navLinks.map(({ id, link, path }) => (
             <li className="navListItem" key={id}>
-              <NavLink to={path}>{link}</NavLink>
+              <NavLink to={path} onClick={() => setIsOpen((prev) => !prev)}>
+                {link}
+              </NavLink>
             </li>
           ))}
         </ul>
-      </div> */}
+        {/* </button> */}
+      </div>
     </nav>
   );
 };
