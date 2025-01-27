2b42b985ac4e49a6838b2da7db0e53561f1b88e1
RamonSanGabriel <jeshero11@gmail.com>
2025-01-28 02:16:20 +0800

    Update toys.js

--- src/data/toys.js
+++ src/data/toys.js
@@ -1,9 +1,9 @@
-import toy1 from '../images/Toys/Toy1.png';
-import toy2 from '../images/Toys/Toy2.png';
-import toy3 from '../images/Toys/Toy3.png';
-import toy4 from '../images/Toys/Toy4.png';
-import toy5 from '../images/Toys/Toy5.png';
-import toy6 from '../images/Toys/Toy6.png';
+import toy1 from '../images/ToysImages/Toy1.png';
+import toy2 from '../images/ToysImages/Toy2.png';
+import toy3 from '../images/ToysImages/Toy3.png';
+import toy4 from '../images/ToysImages/Toy4.png';
+import toy5 from '../images/ToysImages/Toy5.png';
+import toy6 from '../images/ToysImages/Toy6.png';
 
 export const toyDetails = {
   title: 'Toy Category List',
@@ -17,7 +17,7 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy1,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy1.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy1.png',
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy2,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy2.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy2.png',
   },
   {
     id: 3,
@@ -33,7 +33,7 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy3,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy3.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy3.png',
   },
   {
     id: 4,
@@ -41,7 +41,7 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy4,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy4.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy4.png',
   },
   {
     id: 5,
@@ -49,7 +49,7 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy5,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy5.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy5.png',
   },
   {
     id: 6,
@@ -57,6 +57,6 @@ export const toyList = [
     price: 10.99,
     description: 'This is toy',
     image: toy6,
-    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/Toys/Toy6.png',
+    href: 'https://github.com/RamonSanGabriel/kole-trevi/blob/main/src/images/ToysImages/Toy6.png',
   },
 ];
