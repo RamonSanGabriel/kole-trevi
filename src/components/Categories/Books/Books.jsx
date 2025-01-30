2d66dbf4379b9486490afb8ece647510b1d2e203
RamonSanGabriel <jeshero11@gmail.com>
2025-01-30 19:23:20 +0800

    books lazyloadimage

--- src/components/Categories/Books/Books.jsx
+++ src/components/Categories/Books/Books.jsx
@@ -1,7 +1,7 @@
-import './Books.css';
+import css from './Books.module.css';
 import { bookDetails, bookList } from '../../../data/book.js';
-// import { NavLink } from 'react-router-dom';
-// import Lightbox from 'yet-another-react-lightbox';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import { useState } from 'react';
 
 const Books = () => {
@@ -11,24 +11,29 @@ const Books = () => {
   return (
     <>
       {/* <CategoryHome /> */}
-      <div className="bookWrapper">
-        <div className="bookDetails">
+      <div className={css.bookWrapper}>
+        <div className={css.bookDetails}>
           <h2>{title}</h2>
           <p>{description}</p>
         </div>
-        <div className="bookContainer">
-          <ul className="bookList" onClick={() => setOpen(true)}>
-            {bookList.map(({ id, name, price, description, src }) => (
+        <div className={css.bookContainer}>
+          <ul className={css.bookList} onClick={() => setOpen(true)}>
+            {bookList.map(({ id, name, description, image, placeholder }) => (
               <li key={id} onClick={() => setOpen(true)}>
                 <h4>
                   {name}&nbsp;{id}
                 </h4>
-                <div className="bookImage">
-                  {/* <button > */}
-                  {/* <NavLink to={src} target="_blank"> */}
-                  <img src={src} alt={description} />
-                  {/* </NavLink> */}
-                  {/* </button> */}
+                <div className={css.bookImage}>
+                  <div className={css.lazyLoadContainer}>
+                    <LazyLoadImage
+                      className={css.lazyLoad}
+                      key={id}
+                      effect="blur"
+                      src={image}
+                      alt={`toy image ${id}`}
+                      placeholderSrc={placeholder}
+                    />
+                  </div>
                 </div>
                 <p>
                   {description}&nbsp;{id}
--- src/data/book.js
+++ src/data/book.js
@@ -1,3 +1,22 @@
+import book1 from '../images/BookImages/book1.png';
+import book1small from '../images/BookImages/book1-small.png';
+import book2 from '../images/BookImages/book2.png';
+import book2small from '../images/BookImages/book2-small.png';
+import book3 from '../images/BookImages/book3.png';
+import book3small from '../images/BookImages/book3-small.png';
+import book4 from '../images/BookImages/book4.png';
+import book4small from '../images/BookImages/book4-small.png';
+import book5 from '../images/BookImages/book5.png';
+import book5small from '../images/BookImages/book5-small.png';
+import book6 from '../images/BookImages/book6.png';
+import book6small from '../images/BookImages/book6-small.png';
+import book7 from '../images/BookImages/book7.png';
+import book7small from '../images/BookImages/book7-small.png';
+import book8 from '../images/BookImages/book8.png';
+import book8small from '../images/BookImages/book8-small.png';
+import book9 from '../images/BookImages/book9.png';
+import book9small from '../images/BookImages/book9-small.png';
+
 export const bookDetails = {
   title: 'Book Category List',
   description:
@@ -10,6 +29,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book1,
+    placeholder: book1small,
     src: 'https://images.unsplash.com/photo-1632479734663-a742f1c4ef88?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -17,6 +38,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book2,
+    placeholder: book2small,
     src: 'https://plus.unsplash.com/premium_photo-1677187301660-5e557d9c0724?q=80&w=2200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -24,6 +47,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book3,
+    placeholder: book3small,
     src: 'https://images.unsplash.com/photo-1573309463355-c53f9ebea408?q=80&w=2176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -31,6 +56,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book4,
+    placeholder: book4small,
     src: 'https://images.unsplash.com/photo-1573309463410-ed96625bcd16?q=80&w=2176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -38,6 +65,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book5,
+    placeholder: book5small,
     src: 'https://images.unsplash.com/photo-1513001900722-370f803f498d?q=80&w=2624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -45,6 +74,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book6,
+    placeholder: book6small,
     src: 'https://images.unsplash.com/photo-1563417994954-2736db3bf2c9?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -52,6 +83,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book7,
+    placeholder: book7small,
     src: 'https://images.unsplash.com/photo-1604342162684-0cb7869cc445?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -59,6 +92,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book8,
+    placeholder: book8small,
     src: 'https://plus.unsplash.com/premium_photo-1664373233030-9e4f6fee47c5?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
@@ -66,6 +101,8 @@ export const bookList = [
     name: 'Book',
     price: 10.99,
     description: 'This is book',
+    image: book9,
+    placeholder: book9small,
     src: 'https://images.unsplash.com/photo-1526395050546-dc5b0fe8df5f?q=80&w=2624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
