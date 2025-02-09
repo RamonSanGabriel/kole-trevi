a1ef69cf502dc6d4cecea96e9a8ff32a8b653533
RamonSanGabriel <jeshero11@gmail.com>
2025-02-09 20:10:07 +0800

    Update home.js

--- src/data/home.js
+++ src/data/home.js
@@ -4,7 +4,7 @@ export const homeAbout = {
   paragraph1:
     "Thank you for visiting my page! My dad created this website for me so that when I am bigger, I'll be able to see all the memories we created alongside with my mom. On this page, you'll be able to know more about me and I hope that you will also be a part of my journey.",
   paragraph2:
-    'This 2025, I am currently 5 years old and going to school as kindergarten II. Going to school is one of things I look forward to everyday because I enjoy doing all the activities in the classroom and loves being with my teachers and classmates/friends. I consider my school as my second home because it is where all the new learning and experience are happening.',
+    'This 2025, I am currently 5 years old and going to school as kindergarten II. Going to school is one of the things I look forward to everyday because I enjoy doing all the activities in the classroom and loves being with my teachers and classmates/friends. I consider my school as my second home because it is where all the new learning and experience are happening.',
 
   paragraph3: 'Please join me create new memories with our family and friends.',
 };
