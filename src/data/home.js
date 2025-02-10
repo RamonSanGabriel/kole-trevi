9d12906e2f52ef533630938417cb7b3e1218cf1b
RamonSanGabriel <jeshero11@gmail.com>
2025-02-11 01:50:29 +0800

    Update home.js

--- src/data/home.js
+++ src/data/home.js
@@ -2,8 +2,8 @@ export const homeAbout = {
   title: 'About me',
   greeting: "Hi, I'm Trevi!",
   paragraph1:
-    'I’m so glad you stopped by my page. My dad created this website for me so that I can cherish all the memories we’ve made with my mom when I grow up. On this page, you can learn more about me and I hope you’ll join me on this journey.',
+    "I'm so glad you stopped by my page. My dad created this website for me so that I can cherish all the memories we've made with my mom when I grow up. On this page, you can learn more about me and I hope you'll join me on this journey.",
   paragraph2:
-    'I’m five years old and in kindergarten II. School is one of my favorite things to do every day. I love all the activities in the classroom and being with my teachers and classmates. I consider my school my second home because it’s where I learn and have new experiences.',
+    "I'm five years old and in kindergarten II. School is one of my favorite things to do every day. I love all the activities in the classroom and being with my teachers and classmates. I consider my school my second home because it's where I learn and have new experiences.",
   paragraph3: 'Join me in creating new memories with my family and friends.',
 };
